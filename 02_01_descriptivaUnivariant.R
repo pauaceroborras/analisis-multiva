bb9d844c4100f9e2c4a324d3bcaf1accf3f522ac
Nachete30 <160884110+Nachete30@users.noreply.github.com>
2024-02-28 15:10:56 +0100

    Update 02_01_descriptivaUnivariant.R

--- 02_01_descriptivaUnivariant.R
+++ 02_01_descriptivaUnivariant.R
@@ -1,31 +1,4 @@
 # ==============================================================================
-# [AMD]       01_lecturaDades.R
-# 
-# Autor:      , (c) 22.02.2024
-# Llençament: 
-#     source(".../01_lecturaDades.R")
-# ==============================================================================
-# Carreguem les llibreries necesaries
-
-
-# ==============================================================================
-# Llegim la bbdd
-path <- "C:/Users/ignasi.juncadella/Downloads/"
-fitxer <- "datos.csv"
-dades <- read.csv(paste0(path, fitxer)); cat("S'han llegit les dades./n")
-
-# ==============================================================================
-# cambiem el nom de la bbdd
-colnames(dades) <- c("", "", "", "")
-
-# ==============================================================================
-# Guardem la bbdd
-fitxerFin <- "airlines.RData"
-save(dades, file = paste0(path, fitxerFin)); cat("S'ha guardat les dades en", paste0(path, fitxerFin), "/n")
-
-# ==============================================================================
-
-# ==============================================================================
 # [AMD]       02_01_descriptivaUnivariant.R
 # 
 # Autor:      Ignasi Juncadella, (c) 22.02.2024
