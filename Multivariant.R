536df3ae44a234e803f7c27b70715dab09ce8238
marccheca <160883505+marccheca@users.noreply.github.com>
2024-02-29 15:33:21 +0100

    Update Multivariant.R

--- Multivariant.R
+++ Multivariant.R
@@ -1,13 +1,32 @@
+# ==============================================================================
+# [AMD]       01_lecturaDades.R
+# 
+# Autor:      , (c) 22.02.2024
+# Llençament: 
+#     source(".../01_lecturaDades.R")
+# ==============================================================================
+# Carreguem les llibreries necesaries
+library(psych)
+library(ggplot2)  
 
-# Lectura de la base de dades
-
+# ==============================================================================
+# Llegim la bbdd
+path <- "C:/Users/ignasi.juncadella/Downloads/"
 fitxer <- "datos.csv"
-dades <- read.csv2(fitxer, sep = ",")
+dades <- read.csv(paste0(path, fitxer)); cat("S'han llegit les dades./n")
+# ==============================================================================
+# cambiem el nom de la bbdd
+colnames(dades) <- c("", "", "", "")
+# ==============================================================================
+# Guardem la bbdd
+fitxerFin <- "airlines.RData"
+save(dades, file = paste0(path, fitxerFin)); cat("S'ha guardat les dades en", paste0(path, fitxerFin), "/n")
 
+# ==============================================================================
 # Eliminació de la columna ID
 
 dades <- dades[,-1]
-
+# ==============================================================================
 # Transformació de 3 variables discretes a contínues
 
 food <- dades[,12]
@@ -25,24 +44,25 @@ dades$online_boarding <- online
 seat <- dades[,14]
 dec3 <- runif(length(food), min = 0, max = 0.99)
 seat <- seat + dec3
-
 dades$seat_comfort <- seat
 
+# ==============================================================================
 # Passar a numèrica una variable llegida com a caràcter
 
 dades$arrival_delay_in_minutes <- as.numeric(dades$arrival_delay_in_minutes)
 
+# ==============================================================================
 # Declaració d'una nova variable
 
 dades$flight_time_difference <- dades$departure_delay_in_minutes -
   dades$arrival_delay_in_minutes
+dades <- dades[, -22] #eliminem la vartiable arrival_delay_in_minutes
+# ==============================================================================
+# Imputació NAs
 
-dades <- dades[, -22]
+dades <- 
 
-# Eliminar NAs
-
-dades <- dades[-(which(is.na(dades$flight_time_difference))), ]
-
+# ==============================================================================
 # Passar a factor les variables categòriques
 
 dades$Gender <- as.factor(dades$Gender)
@@ -62,6 +82,8 @@ dades$inflight_service <- as.factor(dades$inflight_service)
 dades$cleanliness <- as.factor(dades$cleanliness)
 dades$satisfaction <- as.factor(dades$satisfaction)
 
+
+# ==============================================================================
 # Crear vectors amb els noms de les variables numèriques i categòriques
 
 format <- sapply(dades, class)
@@ -69,18 +91,26 @@ format <- sapply(dades, class)
 varNum <- names(format)[which(format %in% c("numeric","integer"))]
 varCat <- colnames(dades)[which(!colnames(dades) %in% varNum)]
 
+# ==============================================================================
 # Descriptiva univariant de les variables categòriques
 
 for(vC in varCat) {
-  taula <- table(dades[, vC])
-  cat("Taula de freqüències de la variable", vC, "\n")
-  print(taula)
-  cat("\n")
+  # Creem la taula 
+  tabla <- table(dades[, vC])
+  cat(vC, "\n")
+  cat(tabla, "\n")
   
-  barplot(taula, main = paste0("Gràfic de barres de la variable ", vC), 
-          ylab = "Freqüència", col = "skyblue")
+  # creamos el gráfico correspondiente
+  tabla <- data.frame(tabla)
+  
+  grafic <- ggplot(data= tabla, aes(x=Var1, y=Freq)) +
+    geom_bar(stat="identity", fill="steelblue")+
+    geom_text(aes(label=Var1), vjust=-0.3, size=3.5)+
+    theme_minimal()
+  print(grafic)
 }
 
+# ==============================================================================
 # Descriptiva univariant de les variables numèriques
 
 for(vC in varNum){
